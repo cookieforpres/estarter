91ca8725ebe69ecb95a9d24aefce0c91ae9dc953
cookieforpres <cookiedid911@gmail.com>
2024-07-24 10:20:44 -0700

    Commit message

--- frontend/app/users/[id]/page.tsx
+++ frontend/app/users/[id]/page.tsx
@@ -1,18 +1,29 @@
-import getRequestClient from "@/app/lib/getRequestClient";
-import { APIError } from "@/app/lib/client";
+import getRequestClient from "@/app/lib/getRequestClient"
+import { APIError } from "@/app/lib/client"
 
 export default async function UserDetails({
   params,
 }: {
-  params: { id: string };
+  params: { id: string }
 }) {
-  const client = getRequestClient();
-  const response = await client.user.GetUser(params.id);
+  const client = getRequestClient()
+  let response = null
+  try {
+    response = await client.user.GetUser(params.id)
+  } catch (error) {
+    console.log(error)
+  }
 
   return (
     <section>
-      <h1>Detail for {response.user.name}</h1>
-      <p>ID: {response.user.id}</p>
+      {response !== null ? (
+        <>
+          <h1>Detail for {response.user.name}</h1>
+          <p>ID: {response.user.id}</p>
+        </>
+      ) : (
+        <p>You need to login to view this data</p>
+      )}
     </section>
-  );
+  )
 }
